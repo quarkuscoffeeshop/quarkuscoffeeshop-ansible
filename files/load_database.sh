e158e461809392999d24563695958002ed2d1cf1
Paul <pverge@gmail.com>
2020-10-14 13:12:28 -0400

    Updated Mongodb collection to Order

--- files/load_database.sh
+++ files/load_database.sh
@@ -2,7 +2,7 @@
 
 set -x 
 
-mongoimport --db cafedb --collection inventory \
+mongoimport --db cafedb --collection Order \
           --authenticationDatabase cafedb --username ${MONGO_USERNAME} --password ${MONGO_PASSWORD} \
           --drop --file /data/sample_mongo_data.json  --host ${MONGO_CONNECTION_STRING} --port 27017
 

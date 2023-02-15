a4376bed028b0d8f6409fb06ca81b8d24d1e03cc
Tosin Akinosho <takinosh@redhat.com>
2023-02-15 17:14:06 -0500

    udpating code

--- files/quickstart.sh
+++ files/quickstart.sh
@@ -16,11 +16,11 @@ fi
 
 echo "********************************************************************************"
 echo "Current Settings are:"
-echo "ACM_WORKLOADS=${ACM_WORKLOADS}" >> $HOME/env.variables
-echo "AMQ_STREAMS=${AMQ_STREAMS}" >> $HOME/env.variables
-echo "CONFIGURE_POSTGRES=${CONFIGURE_POSTGRES}" >> $HOME/env.variables
-echo "HELM_DEPLOYMENT=${HELM_DEPLOYMENT}" >> $HOME/env.variables
-echo "DELETE_DEPLOYMENT=${DELETE_DEPLOYMENT}" >> $HOME/env.variables
+echo "ACM_WORKLOADS=$(echo $ACM_WORKLOADS)" >> $HOME/env.variables
+echo "AMQ_STREAMS=$(echo $AMQ_STREAMS)" >> $HOME/env.variables
+echo "CONFIGURE_POSTGRES=$(echo $CONFIGURE_POSTGRES)" >> $HOME/env.variables
+echo "HELM_DEPLOYMENT=$(echo $HELM_DEPLOYMENT)" >> $HOME/env.variables
+echo "DELETE_DEPLOYMENT=$(echo $DELETE_DEPLOYMENT)" >> $HOME/env.variables
 echo "MONGODB_OPERATOR=n" >> $HOME/env.variables
 echo "MONGODB=n" >> $HOME/env.variables
 cat $HOME/env.variables
